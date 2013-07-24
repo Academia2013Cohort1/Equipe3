b931e27b68452cb1d05ed1b569fa27a3fc139f0d
academia2013cohort1 <Emile.lafreniere@ubisoft.com>
2013-07-24 10:35:08 -0400

    truc

---
+++ Separating Animations - Unity3D_files/index(4).php
@@ -0,0 +1,9 @@
+/* generated javascript */
+var skin = 'monobook';
+var stylepath = '/unity3d/skins';
+
+/* MediaWiki:Common.js */
+/* Any JavaScript here will be loaded for all users on every page load. */
+
+/* MediaWiki:Monobook.js */
+/* Any JavaScript here will be loaded for users using the MonoBook skin */
